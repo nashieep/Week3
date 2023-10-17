a224524ffb7f817e59d3ecec889f2bf7120901ce
nashieep <nmpongnon@gmail.com>
2023-10-17 13:45:30 -0500

    first commit

---
+++ practice1.js
@@ -0,0 +1,36 @@
+// assign two to the variable a
+a = 2
+// assign 7 to the variable b
+b = 7
+// assign a times b to the variable c
+c = a * b
+// print to the screen the content of c
+console.log(c)
+
+// CTRL-/ AKA Control slash
+username = "Nash" // string to Char
+x = "2"
+y = "7"
+z = x + y // concatenation "chaining" like a train.
+console.log(z)
+//MOVIES
+R = 20
+PG = 10
+age = 5
+if (age >= R) {
+  console.log("CAN see R")
+  }else if (age >= PG) {
+  console.log("CAN see PG")
+  }else{ //DEFAULT IF FALSE
+  console.log("CAN see G")
+  }
+//LOOP
+count = 12
+LIMIT = 0
+
+while (count > LIMIT) {
+  console.log(count) //REPEATS
+  count = count - 1
+}
+
+console.log("BLAST OFF!!!")
